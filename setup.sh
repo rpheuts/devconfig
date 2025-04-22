0fee808c28a83c52a915f775a5478cb10b64a4ba
Robert Heuts <heutsr@amazon.com>
2025-04-22 16:57:59 -0700

    Added examples for adding to shell startup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 # brew install fzf ghostty starship neovim font-cousine-nerd-font
 
 cp -r nvim ~/.config/
-cp -r ghostty ~/.config/ 
+cp -r ghostty ~/.config/
+
+# .bashrc: eval "$(starship init bash)"
+# .zshrc: eval "$(starship init zsh)"
